e14e7dd90bf812d5faa32fbcea11ddfb4efd7a31
nikcinigiani <nik.cinigiani@gmail.com>
2022-07-22 13:14:42 +0200

    Correzioni Classi

--- cmake-build-debug/CMakeFiles/ZombicideLib.dir/Classes/Card.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/ZombicideLib.dir/Classes/Card.cpp.o: \
- /home/gabriele/CLionProjects/Zombicide/Classes/Card.cpp \
- /usr/include/stdc-predef.h \
- /home/gabriele/CLionProjects/Zombicide/Classes/Card.h
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/gabriele/CLionProjects/Zombicide
+# Install script for directory: /Users/nikcinigiani/Documents/progetto/Zombicide
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,11 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -39,12 +34,12 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/gabriele/CLionProjects/Zombicide/cmake-build-debug/test/cmake_install.cmake")
+  include("/Users/nikcinigiani/Documents/progetto/Zombicide/cmake-build-debug/test/cmake_install.cmake")
 
 endif()
 
@@ -56,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/gabriele/CLionProjects/Zombicide/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/nikcinigiani/Documents/progetto/Zombicide/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
